74f73c902f13bf5a6814405a3c49f227196f03b7
MaestroEddy <53114885+MaestroEddy@users.noreply.github.com>
2021-02-25 12:04:46 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,9 +10,9 @@ let package = Package(name: "OkaySDK",
                                               checksum: "bfabff6827a014fa34f6496f59b06bea0480f163bc41fc5145fdc9defce5436c"),
                                 .binaryTarget(name: "ePaymentsUI",
                                               url: "https://github.com/Okaythis/ePaymentsiOS/raw/f748e5c6f7851a762ae716df37ec3b149abaea02/ePaymentsUI.xcframework.zip",
-                                              checksum: "1b95bc6a3415b5905d290a56e036bae426c6c75bd4dcc2cd672e09bb5b3f7ab0"),
+                                              checksum: "c3d79917d9d635f5114429f102c454ab6481679bf21227a78acc74e3bf45faa2"),
                                 .binaryTarget(name: "PSA",
                                               url: "https://github.com/Okaythis/ePaymentsiOS/raw/78edcc1e4a4f250a10f51525c1a21a840f007b71/PSA.xcframework.zip",
-                                              checksum: "fd0b6f916f43e51136096787e79b50c701ce9cd1082b17b86563108002d356e1")
+                                              checksum: "56edf560b9925f871d24d3e3654f05381711035d10464490c5404483dc561532")
                                ]
                      )
